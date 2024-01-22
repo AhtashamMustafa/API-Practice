ada78c23b91a33a4034b379499c820cc5a4dc4e1
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2024-01-22 21:44:06 +0500

    update

--- Backend/Config/config.js
+++ Backend/Config/config.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose"
 
 dotenv.config()
 
+let database= process.env.MONGODB_URI
+
 export const dbConnection = async ()=>{
     try{
-        const con = await mongoose.connect(process.env.MONGODB_URI)
+        const con = await mongoose.connect(database)
         console.log(`MongoDB Connected: ${con.connection.host}`)
     }catch(e){
         console.log("Error ===>",e)}
